352c5730830c39cba5a0005db4e0efdd29cd4ea8
tharun066 <113330534+tharun066@users.noreply.github.com>
2022-09-15 11:24:16 +0530

    Add files via upload

---
+++ involving the appropriate system call.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+
+int main() 
+{ 
+
+  
+
+   
+
+    
+
+  
+
+    printf("Hello world!\n"); 
+
+    return 0; 
+}
---
+++ program 10.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+int main(){
+   char ch;
+   FILE *fp;
+   fp=fopen("std1.txt","w");
+   printf("enter the text.press cntrl Z:\n");
+   while((ch = getchar())!=EOF){
+      putc(ch,fp);
+   }
+   fclose(fp);
+   fp=fopen("std1.txt","r");
+   printf("text on the file:\n");
+   while ((ch=getc(fp))!=EOF){
+      if(ch == ',')
+         printf("\t\t");
+      else
+         printf("%c",ch);
+   }
+   fclose(fp);
+   return 0;
+}
